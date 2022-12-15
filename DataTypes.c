ff7dedd69917266fa81736b18d6fe6bcb8a3be7e
Girish Muley <girish.muley007@gmail.com>
2022-12-15 17:26:47 +0530

    print value of different data types

---
+++ DataTypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	char val1 = 'a';
+    int val2 = 2;
+    float val3 = 2.001;
+    char val4[] = "codingninja"; 
+    
+   printf("%c\n", val1);
+    printf("%d\n", val2); 
+    printf("%5.3f\n", val3);
+    printf("%s\n", val4);
+    return 0;
+}
