d82889395dcf4426e51956edb5583bb91a48a62a
Girish Muley <girish.muley007@gmail.com>
2022-12-17 13:29:39 +0530

    square pattern

---
+++ sqaurepattern.c
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+    cin >> n;
+    int currRow = 1;
+    while(currRow <= n) {
+        int currCol = 1;
+        while(currCol <= n) {
+            cout << n;
+            currCol++;
+        }
+        cout << endl;
+        currRow++;
+    }
+    
+}
