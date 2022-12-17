7a1d342669e25ae839d85eaabc3979d485b506d0
Girish Muley <girish.muley007@gmail.com>
2022-12-17 13:34:40 +0530

    charactor pattern

---
+++ charactorpattern.c
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+    cin >> n;
+    int currRow = 1;
+    while(currRow <= n) {
+        int currCol = 1;
+        char ch = 'A' + currRow - 1;
+        while(currCol <= currRow) {
+            cout << char(ch + currCol - 1);
+            currCol++;
+        }
+        cout << endl;
+        currRow++;
+    }
+    
+}
